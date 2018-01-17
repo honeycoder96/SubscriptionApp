efd5cdffbbe92924cdafd9ab1af34e2a93b27f01
honeycoder96 <softwaretester2016pro@gmail.com>
2018-01-18 05:21:49 +0530

    added login button

---
+++ src/main/java/com/honey/web/controllers/LoginController.java
@@ -0,0 +1,23 @@
+/**
+ * @author honey
+ */
+package com.honey.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author honey
+ *
+ */
+@Controller
+public class LoginController {
+
+	/** The login view name */
+	public static final String LOGIN_VIEW_NAME = "user/login";
+
+	@RequestMapping("/login")
+	public String login() {
+		return LOGIN_VIEW_NAME;
+	}
+}
