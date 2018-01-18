80599f9e4f8b3173f8fd4389b83e760f6d65b1af
honeycoder96 <softwaretester2016pro@gmail.com>
2018-01-18 18:30:48 +0530

    added payload page

---
+++ src/main/java/com/honey/web/controllers/PayLoadController.java
@@ -0,0 +1,22 @@
+/**
+ * @author honey
+ */
+package com.honey.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author honey
+ *
+ */
+@Controller
+public class PayLoadController {
+
+	public static final String PAYLOAD_VIEW_NAME = "payload/payload";
+
+	@RequestMapping("/payload")
+	public String payload() {
+		return PAYLOAD_VIEW_NAME;
+	}
+}
